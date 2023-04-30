1dd64869cfff48ca0ab7d4e6b118880dfdc11029
tochi26 <nzewitochukwu26@gmail.com>
2023-04-30 06:11:36 +0100

    added some functionalities to the checkout page

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Customer, Product,Cart
+from . models import Customer, OrderPlaced, Product, Payment, Cart
 # Register your models here.
 
 @admin.register(Product)
@@ -12,4 +12,12 @@ class CustomerModelAdmin(admin.ModelAdmin):
 
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'product','quantity']    
+    list_display = ['id', 'user', 'product','quantity']   
+
+@admin.register(Payment)
+class PaymentModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','amount','paypal_order_id','paypal_payment_status','paypal_payment_id','paid']    
+
+@admin.register(OrderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','customer','product','ordered_date','status','payment']     
