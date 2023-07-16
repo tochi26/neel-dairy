d9f220912d3c86797ed561a3c97f628157061ede
Tochukwu Nzewi <tochukwunzewi@Tochukwus-MacBook-Air.local>
2023-07-16 12:48:22 +0100

    made updates to the README.md

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a web application built for an e-commerce store that allows users to browse and purchase products. The app is built using Python, Django, javascript, HTML/CSS Bootstrap.
 
-![Image description](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/l1pfs1x2i9bb31cl6mph.png)
+![Image description](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/hhfcpq8697vrymfictlz.png)
 
 ## Installation
 
--- app/views.py
+++ app/views.py
@@ -214,12 +214,13 @@ class checkout(View):
 @login_required
 def orders(request):
     totalitem = 0
-    wishitem=0
+    wishitem = 0
     if request.user.is_authenticated:
         totalitem = len(Cart.objects.filter(user=request.user))
         wishitem = len(Wishlist.objects.filter(user=request.user))
-    order_placed=OrdersPlaced.objects.filter(user=request.user)  
-    return render(request, 'app/oders.html',locals())       
+    order_placed = OrdersPlaced.objects.filter(user=request.user)  
+    return render(request, 'app/orders.html', locals())
+       
 
 def plus_cart(request):
     if request.method == 'GET':
